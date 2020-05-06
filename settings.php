1b93ae37af47e91b2251070feefb2791525fc234
Sam Møller <smo@praxis.dk>
2020-05-06 15:04:13 +0200

    Change settings default meetingsapplink to https://teams-meeting.praxis.dk

--- settings.php
+++ settings.php
@@ -31,7 +31,7 @@ if ($ADMIN->fulltree) {
     // Meeting application link.
     $name = new lang_string('meetingsapplink', 'atto_teamsmeeting');
     $desc = new lang_string('meetingsapplink_desc', 'atto_teamsmeeting');
-    $default = 'https://enovation.ie/msteams';
+    $default = 'https://teams-meeting.praxis.dk';
 
     $setting = new admin_setting_configtext('atto_teamsmeeting/meetingapplink',
                                               $name,
